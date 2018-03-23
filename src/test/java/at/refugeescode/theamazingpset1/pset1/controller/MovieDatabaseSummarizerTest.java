36f5d1c8129aeb0274493007e34542c5afee4cd4
mohammmod <mohammad.sawas2@outlook.com>
2018-03-23 12:41:57 +0100

    cleaning the code

--- src/test/java/at/refugeescode/theamazingpset1/pset1/controller/MovieDatabaseSummarizerTest.java
+++ src/test/java/at/refugeescode/theamazingpset1/pset1/controller/MovieDatabaseSummarizerTest.java
@@ -1,6 +1,6 @@
 package at.refugeescode.theamazingpset1.pset1.controller;
 
-import at.refugeescode.theamazingpset1.pset1.modal.Summary;
+
 import at.refugeescode.theamazingpset1.pset1.parser.ParserActors;
 import at.refugeescode.theamazingpset1.pset1.parser.ParserMovie;
 import org.junit.Assert;
@@ -14,9 +14,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MovieDatabaseSummarizerTest {
 
-    private Summary summary = new Summary();
+
     List<pset1.modal.MovieSummarizer> movieInfo = ParserMovie.getMovieInfo();
     List<pset1.modal.Actor> actorsInfo = ParserActors.getActorsInfo();
+
     @Test
     void testRating() {
         List<String> movieRating = MovieInfoGuy.getRatings(movieInfo);
@@ -27,30 +28,32 @@ class MovieDatabaseSummarizerTest {
         Assert.assertThat(movieRating.get(0), is("Reckless (10.0)"));
         Assert.assertThat(movieRating.get(4), is("Other Voices Other Rooms (10.0)"));
     }
+
     @Test
-    void testGenres(){
+    void testGenres() {
         List<String> movieGenres = MovieInfoGuy.getMovieGenres(movieInfo);
         int size = movieGenres.size();
-        assertEquals(size,2);
-        Assert.assertThat(movieGenres.get(0).trim(),is("Drama (14495)"));
+        assertEquals(size, 2);
+        Assert.assertThat(movieGenres.get(0).trim(), is("Drama (14495)"));
         Assert.assertThat(movieGenres.get(1).trim(), is("Comedy (8955)"));
 
     }
+
     @Test
-    void testMostHiredActors(){
+    void testMostHiredActors() {
         List<String> mostHirdActors = ActorsInfoGuy.getMostHirdActors(actorsInfo);
         int size = mostHirdActors.size();
-        assertEquals(size,5);
-        Assert.assertThat(mostHirdActors.get(0).trim(),is("Bess Flowers (82)"));
+        assertEquals(size, 5);
+        Assert.assertThat(mostHirdActors.get(0).trim(), is("Bess Flowers (82)"));
         Assert.assertThat(mostHirdActors.get(4).trim(), is("Eric Roberts (52)"));
 
     }
-    @Test
-    void testActorsRatio(){
-        String mostHirdActors = ActorsInfoGuy.getRatio(actorsInfo);
-        Assert.assertThat(mostHirdActors.trim(),is("female : (64%) male : (35%)"));
-        //Assert.assertThat(mostHirdActors.trim(), is("Eric Roberts (52)"));
 
+    @Test
+    void testActorsRatio() {
+        String mostHirdActors = ActorsInfoGuy.getRatio(actorsInfo);
+        Assert.assertThat(mostHirdActors.trim(), is("female : (64%) male : (35%)"));
+        
     }
 
 
